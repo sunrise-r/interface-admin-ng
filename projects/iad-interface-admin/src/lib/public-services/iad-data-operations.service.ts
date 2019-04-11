d5bbc0ca6625e1dfc31384502fa30ed3d916c2ef
Sergey Averkiev <sergey.a@sunrise-r.ru>
2019-04-11 10:42:34 +0300

    #15 Convert data to plain object before sending it for saving

--- projects/iad-interface-admin/src/lib/public-services/iad-data-operations.service.ts
+++ projects/iad-interface-admin/src/lib/public-services/iad-data-operations.service.ts
@@ -9,6 +9,18 @@ export class IadDataOperationsService {
   constructor(private http: HttpClient) {}
 
   saveData(postDataUrl: string, data: any): Observable<HttpResponse<any>> {
-    return this.http.post<any>(postDataUrl, data, { observe: 'response' });
+    return this.http.post<any>(postDataUrl, this.plainObject(data), { observe: 'response' });
+  }
+
+  plainObject(data: any): any {
+    const plainData = {};
+    Object.keys(data).forEach(key => {
+      if (data[key] instanceof Object) {
+        Object.assign(plainData, this.plainObject(data[key]));
+      } else {
+        plainData[key] = data[key];
+      }
+    });
+    return plainData;
   }
 }
