47077c7a67fe4dc0f428dce59c25e432e12d6700
Sergey Averkiev <sergey.a@sunrise-r.ru>
2019-04-11 14:11:56 +0300

    #18 Set timezone when sending ISO date for saving

--- projects/iad-interface-admin/src/lib/public-services/iad-data-operations.service.ts
+++ projects/iad-interface-admin/src/lib/public-services/iad-data-operations.service.ts
@@ -15,12 +15,31 @@ export class IadDataOperationsService {
   plainObject(data: any): any {
     const plainData = {};
     Object.keys(data).forEach(key => {
-      if (data[key] instanceof Object) {
+      if (data[key] instanceof Date) {
+        plainData[key] = this.toISOStringWithTimezone(data[key]);
+      } else if (data[key] instanceof Object) {
         Object.assign(plainData, this.plainObject(data[key]));
       } else {
         plainData[key] = data[key];
       }
     });
     return plainData;
+  }
+
+  toISOStringWithTimezone(date: Date) {
+    const tzo = -date.getTimezoneOffset(),
+      dif = tzo >= 0 ? '+' : '-',
+      pad = function(num) {
+        const norm = Math.floor(Math.abs(num));
+        return (norm < 10 ? '0' : '') + norm;
+      };
+    return date.getFullYear() +
+      '-' + pad(date.getMonth() + 1) +
+      '-' + pad(date.getDate()) +
+      'T' + pad(date.getHours()) +
+      ':' + pad(date.getMinutes()) +
+      ':' + pad(date.getSeconds()) +
+      dif + pad(tzo / 60) +
+      ':' + pad(tzo % 60);
   }
 }
