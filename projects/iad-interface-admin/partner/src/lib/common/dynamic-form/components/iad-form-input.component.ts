f181c6263a84f0fde84bcf6018826f6038a68bc1
pavel.a <pavel.a@sunrise-r.ru>
2019-05-20 18:02:42 +0300

    #3 partner/common module updated

--- projects/iad-interface-admin/partner/src/lib/common/dynamic-form/components/iad-form-input.component.ts
+++ projects/iad-interface-admin/partner/src/lib/common/dynamic-form/components/iad-form-input.component.ts
@@ -7,37 +7,38 @@ import { ValidationInput } from '../core/validation-input';
     selector: 'iad-form-input',
     template: `
         <ng-container [formGroup]="group">
-            <label [attr.for]="config.key"
-                   class="col-12 col-lg-{{labelColumnSize}} col-form-label">{{config.label}}: </label>
-            <div class="col-12 col-lg-{{formControlColumnSize}}">
-                <div [ngClass]="{'input-wrapper': config.type !== 'hidden'}">
-                    <ng-container *ngIf="config.inputMask; then maskedInput; else simpleInput"></ng-container>
-                    <ng-template #maskedInput>
-                        <p-inputMask
-                            styleClass="form-control"
-                            [formControlName]="config | formatInputName"
-                            [inputId]="config.key"
-                            [type]="config.type"
-                            [readonly]="config.readonly"
-                            [unmask]="true"
-                            [autoClear]="false"
-                            class="custom-form-control"
-                            characterPattern="[A-Za-zА-Яа-я]"
-                            [mask]="config.inputMask"
-                            (onBlur)="onBlur()">
-                        </p-inputMask>
-                    </ng-template>
-                    <ng-template #simpleInput>
-                        <input class="form-control"
-                            [formControlName]="config | formatInputName"
-                            [id]="config.key"
-                            [type]="config.type"
-                            [readonly]="config.readonly"
-                            [maxLength]="config.validators?.maxLength"
-                            (blur)="onBlur()">
-                    </ng-template>
+            <ng-template [ngIf]="config.type !== 'hidden'" [ngIfThen]="shownTpl" [ngIfElse]="inputTpl"></ng-template>
+            <ng-template #shownTpl>
+                <label [attr.for]="config.key"
+                       class="col-12 col-lg-{{labelColumnSize}} col-form-label">{{config.label}}: </label>
+                <div class="col-12 col-lg-{{formControlColumnSize}}">
+                    <div class="input-wrapper">
+                        <ng-template [ngIf]="config.inputMask" [ngIfThen]="maskedInput" [ngIfElse]="inputTpl"></ng-template>
+                        <ng-template #maskedInput>
+                            <p-inputMask
+                                styleClass="form-control"
+                                [formControlName]="config | formatInputName"
+                                [inputId]="config.key"
+                                [type]="config.type"
+                                [readonly]="config.readonly"
+                                [unmask]="true"
+                                [autoClear]="false"
+                                class="custom-form-control"
+                                characterPattern="[A-Za-zА-Яа-я]"
+                                [mask]="config.inputMask"
+                                (onBlur)="onBlur()">
+                            </p-inputMask>
+                        </ng-template>
+                    </div>
                 </div>
-            </div>
+            </ng-template>
+            <ng-template #inputTpl><input class="form-control"
+                 [formControlName]="config | formatInputName"
+                 [id]="config.key"
+                 [type]="config.type"
+                 [readonly]="config.readonly"
+                 [maxLength]="config.validators?.maxLength"
+                 (blur)="onBlur()"></ng-template>
         </ng-container>`
 })
 export class IadFormInputComponent extends ValidationInput implements OnInit, AfterViewInit {
