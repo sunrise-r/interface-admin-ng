77e41ebe1be630427b2da462b6f8ba122b25fd2f
pavel.a <pavel.a@sunrise-r.ru>
2019-05-21 09:07:19 +0300

    #3 Added partner iad exports

--- projects/iad-interface-admin/partner/src/public_api.ts
+++ projects/iad-interface-admin/partner/src/public_api.ts
@@ -1,24 +1,40 @@
+// #model
 export * from './lib/model/form-projection-field.model';
 export * from './lib/model/projection.model';
 export * from './lib/model/events.models';
 export * from './lib/model/iad.models';
+export * from './lib/model/russian-to-english.constants';
+
+// #service
 export * from './lib/services/presentation.service';
 export * from './lib/services/presentation-loader.service';
 export * from './lib/services/projections-api.service';
 export * from './lib/services/presentation-helper';
 export * from './lib/services/data-chain.service';
 
-export * from './lib/toolbar/toolbar-actions/toolbar-action.directive';
-export * from './lib/toolbar/toolbar-actions/toolbar-actions.component';
-export * from './lib/toolbar/models/toolbar-action.model';
-export * from './lib/toolbar/toolbar.module';
+// #utils
+export * from './lib/utils/iad.helper';
 
-export * from './lib/projection-table/table-toolbar/table-toolbar.component';
+// #primeng
+export * from './lib/iad-primeng/table/iad-scrollable-view.component';
+export * from './lib/iad-primeng/table/iad-table.component';
+export * from './lib/iad-primeng/models';
+export * from './lib/iad-primeng/iad-primeng.module';
 
+// #common (ex-customize)
 export * from './lib/common/tabbed-view/projection-tab.model';
+export * from './lib/common/dynamic-form/core/context-aware';
+export * from './lib/common/dynamic-form/core/validation-input';
+export * from './lib/common/dynamic-form/core/form-input.model';
 export * from './lib/common/tabbed-view/tabbed-view.component';
+export * from './lib/common/dynamic-form/dynamic-form.module';
 export * from './lib/common/iad-common.module';
 
+// #filter-builder (elastic replacement)
+export * from './lib/filter-builder/action/customize-query';
+export * from './lib/filter-builder/filter-builder.service';
+
+// #data-table (data-grid)
 export * from './lib/data-table/data-table/data-table.model';
 export * from './lib/data-table/models/actual-selection.model';
 export * from './lib/data-table/services/data-table-information.service';
@@ -27,21 +43,30 @@ export * from './lib/data-table/data-table/data-table-columns.service';
 export * from './lib/data-table/services/actual-selection-chain.service';
 export * from './lib/data-table/services/selection-buffer.service';
 export * from './lib/data-table/data-table/data-table.component';
+export * from './lib/data-table/services/actual-selection-chain.service';
 export * from './lib/data-table/iad-datatable.module';
 
+// #table toolbar
+export * from './lib/toolbar/toolbar-actions/toolbar-action.directive';
+export * from './lib/toolbar/toolbar-actions/toolbar-actions.component';
+export * from './lib/toolbar/models/toolbar-action.model';
+export * from './lib/toolbar/toolbar.module';
+
+// #projection-table
+export * from './lib/projection-table/toolbar-actions-toggle.service';
 export * from './lib/projection-table/projection-table.component';
+export * from './lib/projection-table/table-toolbar/table-toolbar.component';
 export * from './lib/projection-table/projection-table.module';
 
-export * from './lib/projection-form/projection-form/projection-form.component';
-export * from './lib/projection-form/projection-form.module';
-
+// #settings-table
 export * from './lib/iad-settings-table/iad-settings-table.component';
 export * from './lib/iad-settings-table/iad-settings-table.module';
 
-export * from './lib/iad-primeng/table/iad-scrollable-view.component';
-export * from './lib/iad-primeng/table/iad-table.component';
-export * from './lib/iad-primeng/models';
-export * from './lib/iad-primeng/iad-primeng.module';
+// #projection-form
+export * from './lib/projection-form/services/document-info-buffer.service';
+export * from './lib/projection-form/services/document-operations.service';
+export * from './lib/projection-form/projection-form/projection-form.component';
+export * from './lib/projection-form/projection-form.module';
 
 export * from './lib/iad-framework.module';
 
